44edd2c876a6be9142b532e7135401e7a5d6578c
GlobalAstral <cristianmainenti.03@gmail.com>
2025-01-24 20:13:03 +0100

    Added Literals

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <Tokenizer.hpp>
 #include <Preprocessor.hpp>
+#include <Literal.hpp>
 
 using std::cout;
 using std::endl;
---
+++ src/Literal.cpp
@@ -0,0 +1,76 @@
+#include <Literal.hpp>
+
+bool Literal::isSuffix(char c) {
+  return c == 'f' || c == 'L' || c == 'b' || c == 'd';
+}
+
+bool Literal::hasDot(std::string s) {
+  for (char c : s) 
+    if (c == '.')
+      return true;
+  return false;
+}
+
+Literal::Literal Literal::parseLiteral(std::string s) {
+  char last = s.at(s.size()-1);
+  std::string f2 = s.substr(0, 2);
+  bool floating = hasDot(s);
+  if (f2 == HEX_PREFIX) {
+    if (floating) Errors::error("Cannot have floating hex value");
+    return {LiteralType::hexadecimal, {.h = std::stoull(s, 0, 16)}};
+  }
+  if (floating) {
+    if (!isSuffix(last)) 
+      return {LiteralType::double_floating, {.d = std::stod(s)}};
+    if (last == 'f') 
+      return {LiteralType::floating, {.f = std::stof(s)}};
+  } else {
+    if (!isSuffix(last)) 
+      return {LiteralType::integer, {.i = std::stoi(s, 0, 10)}};
+    switch (last) {
+      case 'L':
+        return {LiteralType::long_int, {.l = std::stoll(s, 0, 10)}};
+      case 'b':
+        return {LiteralType::binary, {.b = std::stoull(s, 0, 2)}};
+      case 'f':
+        return {LiteralType::floating, {.f = std::stof(s)}};
+      case 'd':
+        return {LiteralType::double_floating, {.d = std::stod(s)}};
+      default:
+        return {};
+    }
+  }
+  return {};
+}
+
+std::string Literal::Literal::toString() {
+  char buf[255];
+  switch (this->type) {
+    case LiteralType::binary :
+      sprintf(buf, "%I64u", this->u.b);
+      return "BINARY(" + std::string(buf) + ")";
+    case LiteralType::character :
+      buf[0] = this->u.c;
+      buf[1] = '\0';
+      return "CHARACTER(" + std::string(buf) + ")";
+    case LiteralType::double_floating :
+      sprintf(buf, "%f", this->u.d);
+      return "DOUBLE(" + std::string(buf) + ")";
+    case LiteralType::floating :
+      sprintf(buf, "%f", this->u.f);
+      return "FLOAT(" + std::string(buf) + ")";
+    case LiteralType::hexadecimal :
+      sprintf(buf, "%I64u", this->u.h);
+      return "HEX(" + std::string(buf) + ")";
+    case LiteralType::integer :
+      sprintf(buf, "%d", this->u.i);
+      return "INT(" + std::string(buf) + ")";
+    case LiteralType::long_int :
+      sprintf(buf, "%lld", this->u.l);
+      return "LONG(" + std::string(buf) + ")";
+    case LiteralType::string :
+      return "STRING(" + std::string(this->u.s) + ")";
+    default:
+      return "NULL";
+  }
+}
